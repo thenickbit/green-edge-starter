3c3f7013865225e09087c4882de9dd73da44adf7
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-02 01:42:46 -0300

    Add icons and site configuration

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'next-supabase-shadcn',
+  url: 'https://github.com/nchartiot/next-supabase-shadcn',
+  ogImage: '',
+  description: 'A starter project for NextJS 13 apps using supabase and shadcn-ui',
+  links: {
+    github: 'https://github.com/nchartiot/next-supabase-shadcn',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
