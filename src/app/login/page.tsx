e54bb1838ec9ef1b4e418376675eaba8b755cf17
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-02 01:13:12 -0300

    Configure basic eslint rules

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function Login() {
   const [email, setEmail] = useState('')
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import Link from 'next/link'
+
 import LogoutButton from '../components/LogoutButton'
-import SupabaseLogo from '../components/SupabaseLogo'
 import NextJsLogo from '../components/NextJsLogo'
+import SupabaseLogo from '../components/SupabaseLogo'
 
 const resources = [
   {
