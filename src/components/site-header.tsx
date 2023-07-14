b858db86020f33bc1aa47a92f5aacd5aef13a68a
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-07-14 13:54:46 -0300

    WIP: auth pages

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 import { ThemeToggle } from '@/components/theme-toggle';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
@@ -9,8 +9,8 @@ import { cn } from '@/lib/utils';
 export function SiteHeader() {
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/60 backdrop-blur">
-      <div className="container flex h-14 items-center">
-        <h1 className="font-semibold">next-supabase-shadcn</h1>
+      <div className="container flex h-16 items-center">
+        <h1 className="font-semibold">GreenEdge Starter</h1>
         <div className="flex flex-1 items-center justify-end space-x-2 sm:space-x-4">
           <nav className="flex items-center space-x-1">
             <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
@@ -28,6 +28,11 @@ export function SiteHeader() {
               </div>
             </Link>
             <ThemeToggle />
+            <Button variant="outline" asChild>
+              <Link href="/signin" className='ml-8!'>
+                Sign In <Icons.chevronRight className="ml-1 h-4 w-4" />
+              </Link>
+            </Button>
           </nav>
         </div>
       </div>
